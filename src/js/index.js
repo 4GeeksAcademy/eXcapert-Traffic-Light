7719464edd390f94815cb9b5487b8df0d10078de
eXcapert <130175463+eXcapert@users.noreply.github.com>
2023-07-12 23:10:35 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,36 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+// //import your own components
+// import Home from "./component/home.jsx";
+
+function Main() {
+  const [lightState, setLight] = useState();
+
+  return (
+    <div className="container">
+      <div className="stem"></div>
+      <div className="lightBox">
+        <div
+          className={"light red" + (lightState == "red" ? " glow" : "")}
+          onClick={() => setLight("red")}
+        ></div>
+        <div
+          className={"light yellow" + (lightState == "yellow" ? " glow" : "")}
+          onClick={() => setLight("yellow")}
+        ></div>
+        <div
+          className={"light green" + (lightState == "green" ? " glow" : "")}
+          onClick={() => setLight("green")}
+        ></div>
+      </div>
+    </div>
+  );
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Main />, document.querySelector("#app"));
